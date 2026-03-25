61e4ecc95aa849f11c09802425a8f0d39f60c212
Rafe Milner <milnerrafe@outlook.com>
2026-03-25 12:53:30 +0800

    Added Code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,126 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+type Bindings = {
+  SEQTADM: KVNamespace;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.all("/", async (c) => {
+  let times = await c.env.SEQTADM.get(`count`);
+
+  let html = `<html><head><script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js"></script>
+
+  <style>
+
+  button,
+  input[type="button"],
+  input[type="submit"],
+  input[type="reset"] {
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+    /* Optional: Corrects font smoothing for webkit */
+    -webkit-font-smoothing: inherit;
+    -moz-osx-font-smoothing: inherit;
+    /* Optional: Corrects inability to style clickable input types in iOS */
+    -webkit-appearance: none;
+  }
+
+
+  body {
+    position: relative;
+  }
+
+  .centered-div {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-top-left-radius:300px;
+   border-top-right-radius:300px;
+   border-bottom-left-radius:300px;
+   border-bottom-right-radius:300px;
+   background-color: red;
+   font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;
+   font-size:20px;
+    height: 200px;
+    width: 200px;
+    transition: box-shadow 0.3s ease-in-out;
+
+    }
+
+    .text {
+
+      text-align: center;
+      position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+
+
+    }
+
+    .centered-div:hover {
+    box-shadow: 0px 0px 10px red;
+    }
+
+    .centered-div:active {
+      zoom: 95%;
+
+    }
+
+  </style>
+
+  <style>.htmx-indicator{opacity:0;visibility: hidden} .htmx-request .htmx-indicator, .htmx-request.htmx-indicator{opacity:1;visibility: visible;transition: opacity 200ms ease-in}</style></head><body><div class="parent-container">
+
+  <button hx-post="/clicked" hx-swap="outerHTML" class="centered-div">
+
+
+  <div class="text">
+
+  This button has been clicked ${times}
+
+  </div>
+
+
+  </button>
+
+  </div></body></html>`;
+
+  return c.html(html);
+});
+
+app.all("/clicked", async (c) => {
+  let times = await c.env.SEQTADM.get(`count`);
+
+  let timesnumber = parseInt(times);
+
+  timesnumber++;
+
+  let html = `
+  <button hx-post="/clicked" hx-swap="outerHTML" class="centered-div">
+
+
+  <div class="text">
+
+  This button has been clicked ${timesnumber}
+
+  </div>
+
+
+  </button>
+
+  </div>`;
+
+  c.executionCtx.waitUntil(c.env.SEQTADM.put(`count`, timesnumber.toString()));
+
+  return c.html(html);
+});
+
+export default app;
